4d82fede1b55ff4a81248e7dfc798395d59a0b98
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-03-10 19:08:04 -0500

    main.py
    groundwork for player class

--- player.py
+++ player.py
@@ -1,3 +1,7 @@
 class Player():
-    def __init__(self):
-        pass
+    def __init__(self, player_name, department):
+        self.player_name = player_name
+        self.guessed_letters = []
+        self.department = department
+    def guess_letter(self, letter):
+        self.guessed_letters.append(letter)
