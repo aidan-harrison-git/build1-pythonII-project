35cef44164736f902617f97ea42dacc4af304ffe
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-03-14 12:04:28 -0500

    main.py
    added some instructions

--- player_test.py
+++ player_test.py
@@ -2,9 +2,13 @@ import round
 
 play_again = 'yes'
 
+print('This game will challenge you to guess the name of a professor in your department.')
+print('\nYou will have 12 opportunities to guess a letter. Each guess will be applied to the hidden name.')
+print('\nTo win, reveal the name before you exhaust your guesses!')
+
 while play_again != 'no':
     get_name = input('\nDesignate a username: ')
-    round1 = round.Round(get_name)
-    round1.game.cycle() 
+    curr_round = round.Round(get_name)
+    curr_round.game.cycle() 
     play_again = input('Would you like to play again? yes/no: ').lower()
 
