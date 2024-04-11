7f1c3d8a55bc9580c353cdbca629abd1f994b024
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-04-11 16:31:46 -0500

    implemented CSV functionality for departments

--- names.py
+++ names.py
@@ -1,11 +1,54 @@
 import random
+import csv
 
 class Names():
-    directory = {'art_sci':['Deweese', 'Collins', 'Johnson', 'Davis', 'Barton', 'Kersey', 'Fader', 'Gean', 'Pierce', 'Butterfield', 'Chaffin', 'Barr', 'Spencer', 'Lankford', 'Massey', 'Williams', 'McDonald', 'Mennino', 'Spradlin', 'Rhodes', 'Cunningham'], 
-                'bus':['Vega', 'Greenlee', 'Hibbett', 'Roberson', 'Deffenbaugh', 'Smith', 'Lanciloti', 'Deberry', 'Sewell', 'Johnson', 'Satterfield', 'Prentice', 'Black', 'Gray', 'Mennino'], 
-                'bib':['Sokoloski', 'Gardner', 'Rogers', 'Edwards', 'Powell', 'Cook', 'Parker', 'DeBord', 'Blackwelder', 'Ketchum', 'Coleman', 'Moore', 'South'], 
-                'edu':['Bean', 'Cypress', 'Weaver', 'Watson', 'Mabe', 'Baldwin', 'Fraser', 'Johnson', 'Creecy', 'Helton', 'Taylor', 'Cravens', 'McNeal', 'Malecha']
-    }
+    # directory = {'art_sci':['Deweese', 'Collins', 'Johnson', 'Davis', 'Barton', 'Kersey', 'Fader', 'Gean', 'Pierce', 'Butterfield', 'Chaffin', 'Barr', 'Spencer', 'Lankford', 'Massey', 'Williams', 'McDonald', 'Mennino', 'Spradlin', 'Rhodes', 'Cunningham'], 
+    #             'bus':['Vega', 'Greenlee', 'Hibbett', 'Roberson', 'Deffenbaugh', 'Smith', 'Lanciloti', 'Deberry', 'Sewell', 'Johnson', 'Satterfield', 'Prentice', 'Black', 'Gray', 'Mennino'], 
+    #             'bib':['Sokoloski', 'Gardner', 'Rogers', 'Edwards', 'Powell', 'Cook', 'Parker', 'DeBord', 'Blackwelder', 'Ketchum', 'Coleman', 'Moore', 'South'], 
+    #             'edu':['Bean', 'Cypress', 'Weaver', 'Watson', 'Mabe', 'Baldwin', 'Fraser', 'Johnson', 'Creecy', 'Helton', 'Taylor', 'Cravens', 'McNeal', 'Malecha']
+    # }
+
+    with open('2023-2024 Faculty Information CSV.csv', 'r') as csvfile:
+        directory = {'BS':[],
+            'EDU':[],
+            'MECS':[],
+            'BUS':[],
+            'NSG':[],
+            'BPHS':[],
+            'A&H':[],
+            'CCA':[]
+        }
+        professor_reader = csv.reader(csvfile, delimiter=',')
+
+        index = 0
+
+        for row in professor_reader: 
+            if index == 0:
+                index += 1
+                continue
+            elif row[2] == 'BS':
+                directory['BS'].append(row[0])
+            elif row[2] == 'EDU':
+                directory['EDU'].append(row[0])
+            elif row[2] == 'MECS':
+                directory['MECS'].append(row[0])
+            elif row[2] == 'BUS':
+                directory['BUS'].append(row[0])
+            elif row[2] == 'NSG':
+                directory['NSG'].append(row[0])
+            elif row[2] == 'BPHS':
+                directory['BPHS'].append(row[0])
+            elif row[2] == 'A&H':
+                directory['A&H'].append(row[0])
+            else:
+                directory['CCA'].append(row[0])
+
+
+
+
+
+
+
     def __init__(self, player):    
         self.string_to_guess_list = []
     
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player():
         self.department = self.department_select()
     
     def print_dept_options(self):
-        dept_strings = ['a) Arts & Sciences', 'b) Biblical Studies', 'c) Business', 'd) Education & Behavioral Sciences']
+        dept_strings = ['a) Behavioral Sciences', 'b) Education', 'c) Math, Engineering, & CompSci', 'd) Business', 'e) Nursing', 'f) Bio & Physical Health Sciences', 'g) Arts & Humanities', 'h) Comms & Creative Arts']
         print("\n Select the letter corresponding to your major's department: \n")
         for dept in dept_strings:
             print(f'{dept} \n')
@@ -13,13 +13,21 @@ class Player():
     def department_select(self):
         department_selection = input(self.print_dept_options())
         if department_selection in ['a', 'a ', 'a)', 'a) ', 'A', 'A ', 'A)', 'A) ']:
-            return 'art_sci'
+            return 'BS'
         elif department_selection in ['b', 'b ', 'b)', 'b) ', 'B', 'B ', 'B)', 'B) ']:
-            return 'bib'
+            return 'EDU'
         elif department_selection in ['c', 'c ', 'c)', 'c) ', 'C', 'C ', 'C)', 'C) ']:
-            return 'bus'
+            return 'MECS'
         elif department_selection in ['d', 'd ', 'd)', 'd) ', 'D', 'D ', 'D)', 'D) ']:
-            return 'edu'
+            return 'BUS'
+        elif department_selection in ['e', 'e ', 'e)', 'e) ', 'E', 'E ', 'E)', 'E) ']:
+            return 'NSG'
+        elif department_selection in ['f', 'f ', 'f)', 'f) ', 'F', 'F ', 'F)', 'F) ']:
+            return 'BPHS'
+        elif department_selection in ['g', 'g ', 'g)', 'g) ', 'G', 'G ', 'G)', 'G) ']:
+            return 'A&H'
+        elif department_selection in ['h', 'h ', 'h)', 'h) ', 'H', 'H ', 'H)', 'H) ']:
+            return 'CCA'
         else:
             print('Please try again...')
         return self.print_dept_options()
